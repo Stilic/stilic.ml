412ac2a2398638faa75298b13b9494388ebb17f0
Stilic <lateman-jpeg@outlook.fr>
2023-02-17 20:01:15 +0100

    yipee!!!

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
 $(() => {
+  document.baseTitle = document.title;
   const contentDiv = $("#main");
+  var currentPage;
   function loadPage(id) {
-    const req = new XMLHttpRequest();
-    req.addEventListener("load", () => {
-      contentDiv.html(req.responseText);
-    });
-    req.open("GET", "/pages/" + id + ".html");
-    req.send();
+    if (id != currentPage) {
+      const req = new XMLHttpRequest();
+      req.addEventListener("load", () => {
+        const isHome = id == "home";
+        currentPage = id;
+        document.title = isHome
+          ? document.baseTitle
+          : id.charAt(0).toUpperCase() +
+            id.slice(1).replace("-", " ") +
+            " - " +
+            document.baseTitle;
+        contentDiv.html(req.responseText);
+        window.history.pushState(
+          { html: req.responseText, title: document.title },
+          document.title,
+          new URL(isHome ? "" : id, document.location.origin).href
+        );
+      });
+      req.open("GET", "/pages/" + id + ".html");
+      req.send();
+    }
   }
   function fixPageURL(url) {
     if (url.trim() == "" || url == "/") return "home";
-    else if (url.startsWith("/")) return url.substring(1);
+    else if (url.startsWith("/")) return url.slice(1);
     return url;
   }
   loadPage("home");
+  $(window).on("popstate", (e) => {
+    if (e.state) {
+      contentDiv.html(e.state.html);
+      document.title = e.state.title;
+    }
+  });
   $(document).on("click", "a", (e) => {
     var url = $(e.target).attr("href");
     if (url) {
